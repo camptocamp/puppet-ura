733632bc6626338ef762bb321301ad8cdf71ed29
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-04-30 16:38:12 +0200

    Fix fragments gathering inside property

--- lib/puppet/type/user_right.rb
+++ lib/puppet/type/user_right.rb
@@ -18,11 +18,11 @@ Puppet::Type.newtype(:user_right) do
 
         def fragments
             # Collect fragments that target this resource by name or title.
-            @fragments ||= catalog.resources.map { |resource|
-                next unless resource.is_a?(Puppet::Type.type(:user_right_assignment))
+            @fragments ||= resource.catalog.resources.map { |res|
+                next unless res.is_a?(Puppet::Type.type(:user_right_assignment))
 
-                if resource[:right] == self[:name] || resource[:right] == title
-                    resource
+                if res[:right] == @resource[:name] || res[:right] == @resource[:title]
+                    res
                 end
             }.compact
         end
