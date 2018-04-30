c94e4b1fc2e726ca77daa7f7b2ef053646d0152e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-04-30 16:10:00 +0200

    Implement user_right_assignment type and use it in user_right

--- lib/puppet/type/user_right.rb
+++ lib/puppet/type/user_right.rb
@@ -13,8 +13,29 @@ Puppet::Type.newtype(:user_right) do
         desc 'The user right name'
     end
 
+    def fragments
+        # Collect fragments that target this resource by name or title.
+        @fragments ||= catalog.resources.map { |resource|
+            next unless resource.is_a?(Puppet::Type.type(:user_right_assignment))
+
+            if resource[:right] == self[:name] || resource[:right] == title
+                resource
+            end
+        }.compact
+    end
+
     newproperty(:sid, :array_matching => :all) do
         desc 'List of SIDs to allow for this right'
+
+        def should
+            values = super
+
+            fragments.each do |f|
+                values << f[:sid]
+            end
+
+            values.compact
+        end
 
         def insync?(current)
             provider.sid_in_sync?(current, @should)
---
+++ lib/puppet/type/user_right_assignment.rb
@@ -0,0 +1,13 @@
+Puppet::Type.newtype(:user_right_assignment) do
+    @doc = <<-'EOT'
+    Append users to a user_right resource.
+    EOT
+
+    newparam(:right) do
+        desc 'The right to append users to'
+    end
+
+    newparam(:sid) do
+        desc 'List of SIDs to append to the right'
+    end
+end
